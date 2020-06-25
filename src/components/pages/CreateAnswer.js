5e1c9c3b38817219245c06a18738c8adeb657366
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-06-24 21:05:36 -0700

    add charachter counters

--- src/components/pages/CreateAnswer.js
+++ src/components/pages/CreateAnswer.js
@@ -1,31 +1,76 @@
 import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
+import classnames from "classnames";
+import memoryCards from "../../mock-data.js/memory-cards";
+import { checkIsOver, MAX_CARD_CHARS } from "../../utils/helpers";
+const memoryCard = memoryCards[0];
 
-export default function CreateAnswer() {
-   return (
-      <AppTemplate>
-         <h4 className=" my-4 text-center text-muted">Add an answer</h4>
+export default class CreateAnswer extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         answerText: memoryCard.answer,
+      };
+   }
 
-         <div className="card">
-            <div className="card-body bg-primary">
-               <textarea rows="11" id="answerArea" autoFocus={true}></textarea>
+   setAnswerText(e) {
+      this.setState({ answerText: e.target.value });
+   }
+
+   checkAnswerHasInvalidCharacterCount() {
+      if (
+         this.state.answerText.length > MAX_CARD_CHARS ||
+         this.state.answerText.length === 0
+      ) {
+         return true;
+      } else return false;
+   }
+
+   render() {
+      return (
+         <AppTemplate>
+            <h4 className=" my-4 text-center text-muted">Add an answer</h4>
+
+            <div className="card">
+               <div className="card-body bg-primary">
+                  <textarea
+                     rows="11"
+                     id="answerArea"
+                     onChange={(e) => this.setAnswerText(e)}
+                     autoFocus={true}
+                  ></textarea>
+               </div>
             </div>
-         </div>
 
-         <p className="float-right mb-5 px-4 py-4" id="characterCounter">
-            0/240
-         </p>
+            <p className="float-right mb-5 px-4 py-4" id="characterCounter">
+               <span
+                  className={classnames({
+                     "text-danger": checkIsOver(
+                        this.state.answerText,
+                        MAX_CARD_CHARS
+                     ),
+                  })}
+               >
+                  {this.state.answerText.length}/{MAX_CARD_CHARS}
+               </span>
+            </p>
 
-         <div className="clearfix"></div>
+            <div className="clearfix"></div>
 
-         <Link
-            to="create-imagery"
-            className="btn btn-outline-primary float-right px-6"
-            id="next-button"
-         >
-            Next
-         </Link>
-      </AppTemplate>
-   );
+            <Link
+               to="create-imagery"
+               className={classnames(
+                  "btn btn-outline-primary float-right px-6",
+                  {
+                     disabled: this.checkAnswerHasInvalidCharacterCount(),
+                  }
+               )}
+               id="next-button"
+            >
+               Next
+            </Link>
+         </AppTemplate>
+      );
+   }
 }
--- src/components/pages/CreateImagery.js
+++ src/components/pages/CreateImagery.js
@@ -2,52 +2,92 @@ import React from "react";
 import saveIcon from "../../icons/save.svg";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
+import classnames from "classnames";
+import { checkIsOver, MAX_CARD_CHARS } from "../../utils/helpers";
+import memoryCards from "../../mock-data.js/memory-cards";
+const memoryCard = memoryCards[0];
 
-export default function CreateImagery() {
-   return (
-      <AppTemplate>
-         <h4 className="my-4 text-center text-muted">Add memorable imagery</h4>
+export default class CreateImagery extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         imageryText: memoryCard.imagery,
+      };
+   }
 
-         <div className="card" id="cardInput">
-            <div className="card-body bg-primary">
-               <textarea rows="7" cols="30" autoFocus={true}></textarea>
+   setImageryText(e) {
+      this.setState({ imageryText: e.target.value });
+   }
+
+   checkImageryHasInvalidCharacterCount() {
+      if (
+         this.state.imageryText.length > MAX_CARD_CHARS ||
+         this.state.imageryText.length === 0
+      ) {
+         return true;
+      } else return false;
+   }
+   render() {
+      return (
+         <AppTemplate>
+            <h4 className="my-4 text-center text-muted">
+               Add memorable imagery
+            </h4>
+
+            <div className="card" id="cardInput">
+               <div className="card-body bg-primary">
+                  <textarea
+                     rows="4"
+                     cols="30"
+                     autoFocus={true}
+                     onChange={(e) => this.setImageryText(e)}
+                  ></textarea>
+               </div>
             </div>
-         </div>
-         <div className="card" id="cardText">
-            <div className="card-body bg-secondary ">
-               One morning, when Gregor Samsa woke from troubled dreams, he
-               found himself transformed in his bed into a horrible vermin. He
-               lay on his armour-like back, and if he lifted his head a little
-               he could see his brown belly, slightly domed and divided by
-               arches into stiff sections. The bedding was hardly able to cover
-               it and seemed ready to slide off any moment.
+            <div className="card" id="cardText">
+               <div className="card-body bg-secondary ">
+                  {memoryCard.imagery}
+               </div>
             </div>
-         </div>
 
-         <p className="float-right mb-5">0/240</p>
+            <p className="float-right mb-5">
+               <span
+                  className={classnames({
+                     "text-danger": checkIsOver(
+                        this.state.imageryText,
+                        MAX_CARD_CHARS
+                     ),
+                  })}
+               >
+                  {this.state.imageryText.length}/{MAX_CARD_CHARS}
+               </span>
+            </p>
 
-         <div className="clearfix"></div>
+            <div className="clearfix"></div>
 
-         <Link
-            to="create-answer"
-            className="btn btn-link"
-            id="back-to-answer-imagery"
-         >
-            Back to answer
-         </Link>
-         <Link
-            to="create-answer"
-            className="btn btn-primary btn-lg float-right"
-            id="save-imageryButton"
-         >
-            <img
-               src={saveIcon}
-               width="20px"
-               style={{ marginBottom: "3px", marginRight: "5px" }}
-               alt=""
-            />
-            Save
-         </Link>
-      </AppTemplate>
-   );
+            <Link
+               to="create-answer"
+               className="btn btn-link"
+               id="back-to-answer-imagery"
+            >
+               Back to answer
+            </Link>
+            <Link
+               to="create-answer"
+               className={classnames("btn btn-primary btn-lg float-right", {
+                  disabled: this.checkImageryHasInvalidCharacterCount(),
+               })}
+               id="save-imageryButton"
+            >
+               <img
+                  src={saveIcon}
+                  width="20px"
+                  style={{ marginBottom: "3px", marginRight: "5px" }}
+                  alt=""
+               />
+               Save
+            </Link>
+         </AppTemplate>
+      );
+   }
 }
